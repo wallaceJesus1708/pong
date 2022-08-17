c69678a6fb92969a24af79d803195a5881a3f515
Wallace Amorim <wallaceamorim18@gmail.com>
2022-08-17 08:17:32 -0300

    Criando jogo ponto e player

---
+++ src/pong/Player.java
@@ -0,0 +1,19 @@
+package pong;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Player {
+
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics g) {
+		
+		g.setColor(Color.blue);
+		g.fillRect(100, 120-10, 40, 10);
+		
+	}
+	
+}
